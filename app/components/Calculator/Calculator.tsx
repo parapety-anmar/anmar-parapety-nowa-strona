03aad3665d7d5f3ac391ebdce880844fef6fd39d
Robert <oz.robert@o2.pl>
2024-05-08 10:04:57 +0200

    dodanie generowania wycen i aktualizacja kalkulatora

--- app/components/Calculator/Calculator.tsx
+++ app/components/Calculator/Calculator.tsx
@@ -1,51 +1,197 @@
-'use client'
+'use client';
 
-import X from 'components/shared/icons/X'
-import { useState, ChangeEvent } from 'react'
+import { Onedoc } from '@onedoc/client';
+import { Tailwind, compile } from '@onedoc/react-print';
+import ArrowRight from 'components/shared/icons/ArrowRight';
+import Logo from 'components/shared/logo/Logo';
+import { ChangeEvent, useState } from 'react';
 
 interface Field {
-  length: string
-  width: string
-  quantity: string
+  name: string;
+  length: string;
+  width: string;
+  quantity: string;
+  pricePerSqM: string;
 }
 
 const AreaCalculator = () => {
-  const [fields, setFields] = useState<Field[]>([{ length: '', width: '', quantity: '1' }])
-  const [pricePerSqM, setPricePerSqM] = useState<string>('')
+  const [pricingName, setPricingName] = useState<string>('');
+  const [fields, setFields] = useState<Field[]>([
+    {
+      name: `Pozycja 1`,
+      length: '',
+      width: '',
+      quantity: '1',
+      pricePerSqM: '',
+    },
+  ]);
+  const [pdfGeneration, setPdfGeneration] = useState(false);
+  const [pdfGenerationError, setPdfGenerationError] = useState<string>('');
+  const [pdfGenerationLink, setPdfGenerationLink] = useState<string>('');
 
-  const handleInputChange = (index: number, event: ChangeEvent<HTMLInputElement>) => {
-    const values = [...fields]
-    switch (event.target.name) {
-      case 'length':
-        values[index].length = event.target.value
-        break
-      case 'width':
-        values[index].width = event.target.value
-        break
-      case 'quantity':
-        values[index].quantity = event.target.value
-        break
-      default:
-        console.log('?')
-    }
-    setFields(values)
-  }
+  const handleInputChange = (
+    index: number,
+    event: ChangeEvent<HTMLInputElement>,
+    fieldName: keyof Field,
+  ) => {
+    const values = [...fields];
+    values[index][fieldName] = event.target.value;
+    setFields(values);
+  };
 
   const handleAddFields = () => {
-    setFields([...fields, { length: '', width: '', quantity: '1' }])
-  }
+    const newIndex = fields.length + 1;
+    setFields([
+      ...fields,
+      {
+        name: `Pozycja ${newIndex}`,
+        length: '',
+        width: '',
+        quantity: '1',
+        pricePerSqM: '',
+      },
+    ]);
+  };
 
   const handleRemoveFields = (index: number) => {
-    const values = [...fields]
-    values.splice(index, 1)
-    setFields(values)
-  }
+    const values = [...fields];
+    values.splice(index, 1);
+    setFields(values);
+  };
 
   const totalArea = fields.reduce(
-    (total, field) => total + Number(field.length) * Number(field.width) * Number(field.quantity),
-    0
-  )
-  const totalPrice = totalArea * Number(pricePerSqM)
+    (total, field) =>
+      total +
+      Number(field.length) * Number(field.width) * Number(field.quantity),
+    0,
+  );
+
+  const totalPrice = fields.reduce(
+    (total, field) =>
+      total +
+      Number(field.length) *
+        Number(field.width) *
+        Number(field.quantity) *
+        Number(field.pricePerSqM),
+    0,
+  );
+
+  const generatePDF = async () => {
+    const onedoc = new Onedoc(process.env.NEXT_PUBLIC_ONEDOC as string);
+
+    try {
+      const isValid = fields.every(
+        (field) =>
+          field.length.trim() !== '' &&
+          field.width.trim() !== '' &&
+          field.quantity.trim() !== '' &&
+          field.pricePerSqM.trim() !== '' &&
+          Number(field.length) > 0 &&
+          Number(field.width) > 0 &&
+          Number(field.quantity) > 0 &&
+          Number(field.pricePerSqM) > 0,
+      );
+
+      if (!isValid) {
+        setPdfGenerationError(
+          'Wypełnij wszystkie pola i upewnij się, że żadne pole nie zawiera wartości ujemnej.',
+        );
+        return;
+      }
+      setPdfGenerationError('');
+
+      const doc = {
+        html: await compile(
+          <Tailwind>
+            <div>
+              <div className="mb-12 flex items-end justify-between pb-4">
+                <div>
+                  <h1 className="text-2xl font-bold">
+                    {pricingName.trim() || 'Wycena'}
+                  </h1>
+                  <p className="text-xs">{new Date().toLocaleDateString()}</p>
+                </div>
+                <Logo />
+              </div>
+
+              <div className="text-right">
+                <p className="mb-1 p-0">
+                  <b>AN-MAR Parapety</b>
+                </p>
+                <p className="mb-1 p-0">Stawiguda 11-034,</p>
+                <p className="mb-1 p-0">ul. Fermowa 2</p>
+              </div>
+
+              <table className="my-20 w-full">
+                <tr className="border-b border-gray-300">
+                  <th className="py-2 text-left font-bold">Pozycja</th>
+                  <th className="py-2 text-left font-bold">Wymiar</th>
+                  <th className="py-2 text-left font-bold">Ilość sztuk</th>
+                  <th className="py-2 text-left font-bold">Cena</th>
+                </tr>
+
+                {fields.map((field, index) => (
+                  <tr
+                    key={index}
+                    className="border-b border-gray-300"
+                  >
+                    <td className="py-2">{field.name}</td>
+                    <td className="py-2">
+                      {field.length}m x {field.width}m
+                    </td>
+                    <td className="py-2">{field.quantity}szt</td>
+                    <td className="py-2">
+                      {(
+                        Number(field.length) *
+                        Number(field.width) *
+                        Number(field.quantity) *
+                        Number(field.pricePerSqM)
+                      ).toFixed(2)}
+                      zł
+                    </td>
+                  </tr>
+                ))}
+
+                <tr className="border-b border-gray-300">
+                  <th
+                    scope="row"
+                    colSpan={3}
+                    className="text-left"
+                  >
+                    Całkowita cena
+                  </th>
+                  <td className="py-2 font-bold">
+                    {totalPrice.toFixed(2)}zł netto
+                  </td>
+                </tr>
+              </table>
+
+              <p className="text-sm">
+                Ta wycena ma charakter informacyjny i nie stanowi oferty
+                handlowej ani potwierdzenia zakupu
+              </p>
+            </div>
+          </Tailwind>,
+        ),
+        title: pricingName.trim() || 'Wycena',
+        save: true,
+        test: false,
+        expiresIn: 14,
+      };
+
+      setPdfGeneration(true);
+      setPdfGenerationLink('');
+
+      const { link } = await onedoc.render(doc);
+      setPdfGenerationLink(link);
+      setPdfGeneration(false);
+    } catch (err) {
+      console.log('blad');
+      setPdfGenerationError(
+        'Wystąpił błąd podczas generowania wyceny. Spróbuj ponownie później lub skontaktuj się z nami.',
+      );
+    }
+  };
 
   return (
     <div>
@@ -55,9 +201,15 @@ const AreaCalculator = () => {
           className="my-10 lg:my-4"
         >
           <div className="flex flex-col items-start gap-x-4 gap-y-2 lg:flex-row lg:items-center">
-            <div className="flex w-full justify-between gap-2 lg:w-auto">
+            <div className="flex w-full items-center justify-between gap-2 lg:w-auto">
               <span className="pr-4 text-center text-xs font-semibold text-primary-500">
-                Pozycja {index + 1}
+                <input
+                  type="text"
+                  name="position"
+                  value={field.name}
+                  onChange={(event) => handleInputChange(index, event, 'name')}
+                  className={`block w-[120px] rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
+                />
               </span>
               <button
                 onClick={() => handleRemoveFields(index)}
@@ -72,7 +224,7 @@ const AreaCalculator = () => {
                 type="number"
                 name="length"
                 value={field.length}
-                onChange={(event) => handleInputChange(index, event)}
+                onChange={(event) => handleInputChange(index, event, 'length')}
                 className={`block rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
               />
             </label>
@@ -82,7 +234,7 @@ const AreaCalculator = () => {
                 type="number"
                 name="width"
                 value={field.width}
-                onChange={(event) => handleInputChange(index, event)}
+                onChange={(event) => handleInputChange(index, event, 'width')}
                 className={`block rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
               />
             </label>
@@ -92,14 +244,32 @@ const AreaCalculator = () => {
                 type="number"
                 name="quantity"
                 value={field.quantity}
-                onChange={(event) => handleInputChange(index, event)}
+                onChange={(event) =>
+                  handleInputChange(index, event, 'quantity')
+                }
+                className={`block w-[100px] rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
+              />
+            </label>
+            <label className="flex w-full items-center gap-4 text-sm font-normal lg:w-auto lg:flex-col lg:items-start lg:gap-0 2xl:flex-row 2xl:items-center 2xl:gap-2">
+              Cena za m²
+              <input
+                type="number"
+                name="pricePerSqM"
+                value={field.pricePerSqM}
+                onChange={(event) =>
+                  handleInputChange(index, event, 'pricePerSqM')
+                }
                 className={`block w-[100px] rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
               />
             </label>
             <p className="text-sm">
               Częściowa powierzchnia:{' '}
               <span className="text-base font-semibold">
-                {(Number(field.length) * Number(field.width) * Number(field.quantity)).toFixed(3)}{' '}
+                {(
+                  Number(field.length) *
+                  Number(field.width) *
+                  Number(field.quantity)
+                ).toFixed(3)}{' '}
                 m²
               </span>
             </p>
@@ -113,15 +283,6 @@ const AreaCalculator = () => {
         </div>
       ))}
       <div className="flex items-center justify-between gap-4">
-        <label className="flex items-center gap-2 text-sm font-semibold ">
-          Cena za m²
-          <input
-            type="number"
-            value={pricePerSqM}
-            onChange={(e) => setPricePerSqM(e.target.value)}
-            className={`block w-[100px] rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
-          />
-        </label>
         <button
           onClick={() => handleAddFields()}
           className="my-4 block rounded-md bg-primary-500 px-4 py-2 text-center text-sm text-white shadow outline-none duration-200 hover:bg-primary-400 focus-visible:bg-primary-400 active:bg-primary-600"
@@ -132,14 +293,56 @@ const AreaCalculator = () => {
       <div className="mt-6 space-y-2">
         <p>
           Całkowita powierzchnia:{' '}
-          <span className="text-base font-semibold">{totalArea.toFixed(3)} m²</span>
+          <span className="text-base font-semibold">
+            {totalArea.toFixed(3)} m²
+          </span>
         </p>
         <p>
-          Całkowita cena: <span className="text-base font-semibold">{totalPrice.toFixed(2)}zł</span>
+          Całkowita cena:{' '}
+          <span className="text-base font-semibold">
+            {totalPrice.toFixed(2)}zł netto
+          </span>
         </p>
       </div>
+      <div className="mt-20 rounded-md bg-gray-200 px-4 py-2">
+        <div className="flex flex-col items-center  md:flex-row md:justify-between">
+          <label className="flex items-center gap-2 text-sm font-semibold ">
+            Opcjonalny tytuł wyceny
+            <input
+              type="text"
+              value={pricingName}
+              onChange={(e) => setPricingName(e.target.value)}
+              className={`block w-[100px] rounded-md border border-gray-300 bg-gray-50 p-2 text-sm font-light shadow outline-none duration-200 focus-visible:bg-gray-100 focus-visible:ring-2 focus-visible:ring-gray-300 disabled:cursor-not-allowed disabled:opacity-70`}
+            />
+          </label>
+          <button
+            onClick={() => generatePDF()}
+            disabled={pdfGeneration}
+            className="my-4 block rounded-md bg-primary-500 px-4 py-2 text-center text-sm text-white shadow outline-none duration-200 hover:bg-primary-400 focus-visible:bg-primary-400 active:bg-primary-600 disabled:cursor-wait"
+          >
+            Wygeneruj wycenę jako plik PDF
+          </button>
+        </div>
+      </div>
+      {pdfGenerationLink && (
+        <a
+          href={pdfGenerationLink}
+          target="_blank"
+          rel="noopener"
+          className="mt-4 flex items-center gap-2 text-[#0969DA] underline"
+        >
+          Wygenerowana wycena
+          <ArrowRight
+            size={24}
+            weight="regular"
+          />
+        </a>
+      )}
+      {pdfGenerationError && (
+        <p className="mt-4 text-error-500">{pdfGenerationError}</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default AreaCalculator
+export default AreaCalculator;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
-import Header from "components/Header/Header";
-import TemporaryInfo from "components/TemporaryInfo/TemporaryInfo";
-import "globals.css";
-import { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import { ReactNode } from "react";
+import { GoogleAnalytics } from '@next/third-parties/google';
+import Header from 'components/Header/Header';
+import TemporaryInfo from 'components/TemporaryInfo/TemporaryInfo';
+import 'globals.css';
+import { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import { ReactNode } from 'react';
 
 const font = Poppins({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
-  title: "AN-MAR Parapety",
+  title: 'AN-MAR Parapety',
   description:
-    "Zapraszamy do odkrycia bogatej oferty parapetów AN-MAR - wiodącego producenta i sprzedawcy parapetów zewnętrznych, parapetów wewnętrznych oraz okien.",
+    'Zapraszamy do odkrycia bogatej oferty parapetów AN-MAR - wiodącego producenta i sprzedawcy parapetów zewnętrznych, parapetów wewnętrznych oraz okien.',
 };
 
 interface RootLayoutProps {
@@ -25,7 +25,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="pl">
       <body className={`${font.className} bg-gray-50 text-gray-800`}>
-        <TemporaryInfo />
+        {/* <TemporaryInfo /> */}
         <Header />
         {children}
       </body>
